b8af9d62f29c600af240c28c0095e313b7fcccf4
Diana Wendt <dianakw@gmail.com>
2020-01-13 22:24:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,13 @@ def map_to_no_change(source_array)
   new_array
 end
 
+def map_to_double(source_array)
+  i = 0
+  double_array = []
+  while i < source_array.length do
+    double_array << source_array[i]*2
+    i += 1
+  end
+  double_array
+end
+
