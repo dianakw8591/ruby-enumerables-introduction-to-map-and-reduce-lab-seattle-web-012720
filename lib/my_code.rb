1b08a342d5b48fa6066662f80e56c80b4f0d0ee5
Diana Wendt <dianakw@gmail.com>
2020-01-13 23:07:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,4 +52,10 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i]
+    if source_array[i] == false
+      return false
+    end
+    i += 1
+  end
+  true
+end
