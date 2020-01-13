cf240597c1189f2a3af221e8b45035832ce802e8
Diana Wendt <dianakw@gmail.com>
2020-01-13 22:20:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,12 @@
 # My Code here....
 
+def map_to_negativize(source_array)
+  i = 0
+  neg_array = []
+  while i < source_array.length do
+    neg_array << -source_array[i]
+    i += 1
+  end
+end
+
+  
